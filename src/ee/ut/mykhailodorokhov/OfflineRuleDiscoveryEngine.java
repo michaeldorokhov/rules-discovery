ca095d966ab41e50658f8ab241466453ce461d89
Mykhailo Dorokhov <mykhailo.dorokhov@ut.ee>
2018-05-25 09:19:13 +0300

    dummy rules discovery engine added

---
+++ src/ee/ut/mykhailodorokhov/OfflineRuleDiscoveryEngine.java
@@ -0,0 +1,10 @@
+package ee.ut.mykhailodorokhov;
+
+import ee.ut.mykhailodorokhov.data.EventLog;
+
+public class OfflineRuleDiscoveryEngine {
+
+    public OfflineRuleDiscoveryEngine() {
+
+    }
+}
